ba0d302a5a5ceec4e071ede6fe44740b04599853
백주연 <juurom@baegjuyeon-ui-MacBookAir.local>
2023-04-30 22:39:55 +0900

    fix: metadata 수정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,13 @@ export default function Document() {
   
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta property="og:url" content="https://aflat.vercel.app/"/>
+        <meta property="og:title" content="Aflat 에이플랫"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:image" content="../static/mainpage.jpeg"/>
+        <meta property="og:description" content="Aflat music studio & lesson"/>
+      </Head>
       <body>
         <Main />
         <NextScript />
