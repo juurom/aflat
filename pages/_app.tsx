ff406890cf1ddf0bd0437e8e11781e8050ca96da
백주연 <juurom@baegjuyeon-ui-MacBookAir.local>
2023-04-30 22:36:50 +0900

    fix: metadat 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,11 @@ export default function App({ Component, pageProps }: AppProps) {
   <Head>
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
     <link rel="icon" href="favicon.ico" type="image/x-icon"></link>
+    <meta property="og:url" content="https://aflat.vercel.app/"/>
+    <meta property="og:title" content="Aflat 에이플랫"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:image" content="../static/mainpage.jpeg"/>
+    <meta property="og:description" content="Aflat music studio & lesson"/>
   </Head>
     <Component {...pageProps} />
   </>
